8b51860458ddcbc61aac4be81f8b4a41b768fd9d
qdnqn <adnn.selimovic@gmail.com>
2024-09-30 17:32:02 +0200

    Deploy ebs csi

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,7 +59,7 @@ locals {
 
   aws_addons = {
     enable_cert_manager                          = try(var.addons.enable_cert_manager, false)
-    enable_aws_efs_csi_driver                    = try(var.addons.enable_aws_efs_csi_driver, true)
+    enable_aws_efs_csi_driver                    = try(var.addons.enable_aws_efs_csi_driver, false)
     enable_aws_fsx_csi_driver                    = try(var.addons.enable_aws_fsx_csi_driver, false)
     enable_aws_cloudwatch_metrics                = try(var.addons.enable_aws_cloudwatch_metrics, false)
     enable_aws_privateca_issuer                  = try(var.addons.enable_aws_privateca_issuer, false)
